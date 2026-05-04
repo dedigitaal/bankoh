47548f6067963afc31542d606e11ec0c56790960
dedigitaal <spreek@dedigitaal.nl>
2026-05-04 13:40:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -645,4 +645,49 @@ function initLogoReveal(scope) {
   });
 
   wrapper.dataset.logoInit = 'true';
+
+// -----------------------------------------
+// BANKOH about text
+// -----------------------------------------
+  gsap.registerPlugin(ScrollTrigger, SplitText)
+  
+function initHighlightText(){
+
+  let splitHeadingTargets = document.querySelectorAll("[data-highlight-text]")
+  splitHeadingTargets.forEach((heading) => {
+    
+    const scrollStart = heading.getAttribute("data-highlight-scroll-start") || "top 90%"
+    const scrollEnd = heading.getAttribute("data-highlight-scroll-end") || "center 40%"
+    const fadedValue = heading.getAttribute("data-highlight-fade") || 0.2 // Opacity of letter
+    const staggerValue =  heading.getAttribute("data-highlight-stagger") || 0.1 // Smoother reveal
+    
+    new SplitText(heading, {
+      type: "words, chars",
+      autoSplit: true,
+      onSplit(self) {
+        let ctx = gsap.context(() => {
+          let tl = gsap.timeline({
+            scrollTrigger: {
+              scrub: true,
+              trigger: heading, 
+              start: scrollStart,
+              end: scrollEnd,
+            }
+          })
+          tl.from(self.chars,{
+            autoAlpha: fadedValue,
+            stagger: staggerValue,
+            ease: "linear"
+          })
+        });
+        return ctx; // return our animations so GSAP can clean them up when onSplit fires
+      }
+    });    
+  });
+}
+
+// Initialize Highlight Text on Scroll
+document.addEventListener("DOMContentLoaded", () =>{
+  initHighlightText();
+});
 }
